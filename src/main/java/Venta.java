79ee29a4ec4b08fe5263c7e365798d03798d56ac
punchslow <dalbit.qilin@gmail.com>
2026-04-27 12:52:20 +0200

    clases stock (y venta)

--- src/main/java/Venta.java
+++ src/main/java/Venta.java
@@ -1,4 +1,36 @@
+import java.time.LocalDate;
 
 public class Venta {
+    public LocalDate fecha;
+    public Producto productoAsociado;
+    public Maquina maquinaAsociada;
 
+    public Venta() {
+
+    }
+    public void registrarVenta(Producto producto, Maquina maquina, LocalDate fecha) {
+        if(producto == null) {
+            System.out.println("Error: El producto no puede ser nulo.");
+            return;
+        }
+        if(maquina == null) {
+            System.out.println("Error: La máquina no puede ser nula.");
+            return;
+        }
+        if(fecha == null) {
+            System.out.println("Error: La fecha no puede ser nula.");
+            return;
+        }
+        this.productoAsociado = producto;
+        this.maquinaAsociada = maquina;
+        this.fecha = fecha;
+    }
+
+    public LocalDate getFecha() {
+        return this.fecha;
+    }
+
+    public Producto getProductoAsociado() {
+        return this.productoAsociado;
+    }
 }
