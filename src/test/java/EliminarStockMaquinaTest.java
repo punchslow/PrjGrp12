4ab3ede9a30a0d6ebedf6d11debaf2f690632915
punchslow <dalbit.qilin@gmail.com>
2026-05-11 11:48:54 +0200

    añadidas clases de prueba

---
+++ src/test/java/EliminarStockMaquinaTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EliminarStockMaquinaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
